b6da4677edf16ba07b6658b8830593f137afd878
lsh745 <lsh745@naver.com>
2024-06-20 02:00:35 +0000

    Initial and temporal commit

---
+++ whisper_k8s/app/annote.py
@@ -0,0 +1,83 @@
+import os
+import time
+import torch
+import json
+from pyannote.audio import Pipeline
+
+class Annotation:
+    def __init__(
+        self,
+        audio_list: list,
+        pipeline_model: str = "pyannote/speaker-diarization-3.1",
+        use_auth_token: str = "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",
+        save_dir: str = "/app/data"
+        ):
+        print("[+] INITIALIZING DIARIZATION")
+        start = time.time()
+
+        self.audio_list = audio_list
+        self.save_dir = save_dir
+
+        self.pipeline = Pipeline.from_pretrained(
+        pipeline_model,
+        use_auth_token=use_auth_token
+        )
+
+        self.pipeline.to(torch.device("cuda"))
+        print("[-] INITIALIZATION COMPLETE.\n\tTIME TAKEN:", time.time() - start)
+
+
+    def load_audio(self, audio_path: str):
+        print("[+] LOADING AUDIO:", audio_path)
+        start = time.time()
+        self.audio_path = audio_path
+        self.diarization = self.pipeline(audio_path)
+        print("[-] LOADING COMPLETE.\n\tTIME TAKEN:", time.time() - start)
+
+
+    def annote(self):
+        print("[+] DIARIZATING")
+
+        start = time.time()
+        result = []
+
+        for turn, _, speaker in self.diarization.itertracks(yield_label=True):
+            # print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
+            result.append({"start": turn.start, "stop": turn.end, "speaker": speaker})
+
+        self.save_data(result)
+        print("[-] DIARIZATION COMPLETE\n\tTIME TAKEN:", time.time() - start)
+        return result
+
+
+    def run(self):
+        annotation_result = []
+
+        for audio_path in self.audio_list:
+            self.load_audio(audio_path = audio_path)
+            annotation_result.append(self.annote())
+
+        return annotation_result
+
+
+    def save_data(self, result):
+        print("[+] SAVING DATA")
+        start = time.time()
+        file_name = os.path.splitext(os.path.basename(self.audio_path))[0]       
+
+        if not os.path.isdir(self.save_dir): 
+            os.makedirs(self.save_dir)
+
+        with open(os.path.join(self.save_dir, f"{file_name}_annotation.json"), "a", encoding='UTF-8') as f:
+            f.write(json.dumps(result))
+
+        with open(os.path.join(self.save_dir, f"{file_name}_annotation.txt"), "a") as f:
+            f.write("".join([f"Start: {i['start']}, Stop: {i['stop']}, Speaker: {i['speaker']}" for i in result]))
+
+        print("[+] DONE SAVING\n\tTIME TAKEN:", time.time() - start)
+
+
+
+if __name__ == "__main__":
+    annotation = Annotation(["/app/data/0603_chairman/0603-01.wav"])
+    print(annotation.run())
---
+++ whisper_k8s/app/whisper_api.py
@@ -0,0 +1,96 @@
+from speech import Speech
+from utils import multi_ext_glob, ext_conversion, archive_dir
+from slack_utils import SlackSDK
+import io
+import os
+import time
+import uvicorn
+from fastapi import FastAPI, Request, Header, Response, status, BackgroundTasks, APIRouter
+from fastapi.responses import JSONResponse
+from starlette.middleware.cors import CORSMiddleware
+from openai import OpenAI
+
+
+class InferenceRequest:
+    def __init__(self):
+        print("INITIALIZING API SERVER")
+        self.router = APIRouter()
+        self.router.add_api_route("/api/stt", self.stt, methods=["POST"])
+        self.slack_sdk = SlackSDK()
+        self.gpt_client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"),)
+
+
+    def stt(
+        self, 
+        request_body: dict
+        ):
+        print("GPT_MODEL:", request_body["gpt_model"])
+        print("MESSAGES:", request_body["messages"])
+        print("LANGUAGE:", request_body["language"])
+        print("AUDIO:", request_body["upload_data_path"])
+
+        language_dict = {
+            "한국어": "ko",
+            "영어": "en",
+            "자동": None
+        }
+
+        speech_tool = Speech(
+            save_dir=request_body["upload_data_path"],
+            language=language_dict[request_body["language"]]
+            )
+
+        speech_tool.video_list = multi_ext_glob(request_body["upload_data_path"], request_body["video_ext_list"], recursive=True)
+        speech_tool.audio_list = multi_ext_glob(request_body["upload_data_path"], request_body["audio_ext_list"], recursive=True)
+        for audio in speech_tool.audio_list:
+            speech_tool.convert_to_wav(audio)
+        speech_tool.wav_list = multi_ext_glob(request_body["upload_data_path"], ["wav"], recursive=True)
+        print(f"[*]\tVIDEO EXTENSIONS: {request_body['video_ext_list']}\n\tLENGTH OF VIDEOS: {len(speech_tool.video_list)}")
+        print(f"[*]\tAUDIO EXTENSIONS: {request_body['audio_ext_list']}\n\tLENGTH OF AUDIOS: {len(speech_tool.audio_list)}")
+        print(f"[*]\TARGET EXTENSIONS: ['wav']\n\tLENGTH OF WAVS: {len(speech_tool.wav_list)}")
+    
+        whisper_result = speech_tool.run()
+        request_body["messages"].append({"role": "user", "content": whisper_result[0]["text"]})
+
+        response = self.gpt_client.chat.completions.create(
+            model=request_body["gpt_model"],
+            messages=request_body["messages"]
+        )
+        print("[*] CHATGPT RESPONSE:", response)
+
+        print("[*] DONE ARCHIVING", request_body["upload_data_path"])
+        
+        print("response.choices[0].message.content", response.choices[0].message.content)
+        print("FILE EXISTS:", os.listdir(request_body["upload_data_path"]))
+        # print(f"PAYLOAD:\n\tmessage={response.choices[0].message.content}\n\tfile_path={os.path.join(request_body['upload_data_path'], 'archive.zip')}\n\tchannel={request_body['slack_id']}")
+        # os.system(f"cp {os.path.join(request_body['upload_data_path'], 'archive.zip')} .")
+        self.slack_sdk.send_message_multiple_files(
+            message=response.choices[0].message.content,
+            file_path_list=[os.path.join(request_body["upload_data_path"], filename) for filename in os.listdir(request_body["upload_data_path"])],
+            channel=request_body["slack_id"]
+        )
+
+    def runserver(self):
+        print("RUNSERVER")
+        app = FastAPI()
+
+        origins = ["*"]
+        app.add_middleware(
+            CORSMiddleware,
+            allow_origins=origins,
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
+
+        app.include_router(self.router)
+        uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("API_PORT")))
+
+
+if __name__ == "__main__":
+    # xtts = XTTS()
+#     minio = minio_api.MinIO()
+    # inference_api = InferenceRequest(xtts, minio)
+    inference_api = InferenceRequest()
+    inference_api.runserver()
+
